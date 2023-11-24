931248f6113be2ee9be0bdefba5ffddf53b02c9e
Robin Simonot <simonot.robin@gmail.com>
2023-11-24 15:47:57 +0100

    Pokeball as Loader

--- src/components/views/Loader/Loader.tsx
+++ src/components/views/Loader/Loader.tsx
@@ -1,5 +1,10 @@
 import "./Loader.css";
+import Pokeball from "../../../assets/pokeball.svg";
 
 export default function Loader() {
-  return <div className="loader-container">LOADING ...</div>;
+  return (
+    <div className="loader">
+      <img src={Pokeball} alt="pokeball" />
+    </div>
+  );
 }
